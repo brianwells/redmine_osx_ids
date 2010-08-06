d08be3445a361d34fd90dc0788f6203faa98ea7f
Brian D. Wells <spam_brian@me.com>
2010-08-05 22:35:37 -0400

    Fixed memory handling of NSImage, NSBitmapImageRep
    Now making copy of image returned by name before
    any modifications since this is a shared object.
    
    No need to release Cocoa objects since this is
    automatically handled by RubyCocoa.

--- lib/redmine_osx_ids/hooks.rb
+++ lib/redmine_osx_ids/hooks.rb
@@ -9,7 +9,7 @@ module RedmineOsxIds
                 require 'ftools'
                 File.makedirs File.dirname(image_path)
                 require 'osx/cocoa'
-                image = OSX::NSImage.imageNamed(OSX::NSImageNameNetwork)
+                image = OSX::NSImage.imageNamed(OSX::NSImageNameNetwork).copy
                 image.setScalesWhenResized true
                 image.setSize OSX::NSMakeSize(16, 16)
                 newimg = OSX::NSImage.alloc.initWithSize(OSX::NSMakeSize(16,16))
@@ -19,8 +19,6 @@ module RedmineOsxIds
                 rep = OSX::NSBitmapImageRep.alloc.initWithData(newimg.TIFFRepresentation)
                 png = rep.representationUsingType_properties(OSX::NSPNGFileType,nil)
                 png.writeToFile_atomically(image_path,true)
-                rep.release
-                newimg.release
             end
             
             return stylesheet_link_tag("#{Redmine::Utils.relative_url_root}/plugin_assets/redmine_osx_ids/stylesheets/redmine_osx_ids.css")
